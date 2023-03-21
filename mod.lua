565f7c99e4ff05602a19066fa496a878fe8672fe
Florian Thiel <flth@flth.de>
2023-03-21 19:07:34 +0100

    initial

---
+++ mod.lua
@@ -0,0 +1,72 @@
+﻿function data()
+return {
+	info = {
+		minorVersion = 1,
+		severityAdd = "NONE",
+		severityRemove = "WARNING",
+		name = _("mod_name"),
+		description = _("mod_desc"),
+		params = {			
+			{
+				key = "endless_mode",
+				name = _("Endlosmodus"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },				
+				defaultIndex = 0,	
+			},
+			{
+				key = "pr_signal_waypoint",
+				name = _("Wegpunkte"),
+				uiType = "CHECKBOX",
+				values = { "No", "Yes", },				
+				defaultIndex = 1,	
+			},
+		},
+		modid = "modwerkstatt_pr_Signale_1",
+		authors = {
+			{
+				name = "ModWerkstatt",
+				role = "CREATOR",	
+			}, 
+		},
+		url = "",		
+		steamId = 0, 
+		tags = { "europe", "preussen", "signal", "prussian" },  		
+		dependencies = {},
+	},
+
+	runFn = function (settings, modParams)
+		
+		local function waypointFilter(fileName, data)		
+			if data.metadata.signal and data.metadata.prSignale and data.metadata.prSignale.waypoint == true then		
+				data.metadata.availability.yearFrom = 1
+				data.metadata.availability.yearTo = 2
+				--return false
+			end
+			--return true
+			return data
+		end
+		local function endlessFilter(fileName, data)	
+			if data.metadata.signal and data.metadata.prSignale then
+                data.metadata.availability.yearTo = 0					
+            end
+			return data	
+		end 
+
+		if modParams[getCurrentModId()] ~= nil then
+			local params = modParams[getCurrentModId()]					
+			if params["pr_signal_waypoint"] == 0 then				
+				--addFileFilter("model/signal", waypointFilter)					
+				addModifier("loadModel", waypointFilter)
+			end
+			if params["endless_mode"] == 1 then				
+				addModifier("loadModel", endlessFilter)
+			end
+			
+		else
+			--addFileFilter("model/signal", waypointFilter)		
+			addModifier("loadModel", endlessFilter)
+		end	
+	end
+}
+end
---
+++ strings.lua
@@ -0,0 +1,46 @@
+function data()
+return {
+	en = {
+		["mod_name"] = "Prussian form signals",
+		["mod_desc"] = "Early signals from the KPEV. In the past, the signals were not used to indicate speed, but to secure the routes. Therefore, there were up to 3-wing entry signals that signaled the journeys into different tracks. Above all, the uppermost wing was important, as it enabled the journey or indicated stop. Therefore the distant signal only had to announce this term. From 1910 the signal order changed and new paintwork was introduced.\nThere were no gas bottles for signal lighting yet. During the day the kerosene lamps were turned down and extinguished. They only burned on the mast in the dark or during the day when the weather was poor.",
+
+		["prSig1_name"] = "Prussian signal - 1 Flügel",
+		["prSig1_desc"] = "Signals stop / drive",
+        ["prSig2_name"] = "Prussian signal - 2 Flügel",
+		["prSig2_desc"] = "Signals stop - drive/junction",
+        ["prSig21_name"] = "Prussian signal - 2 Flügel",
+		["prSig21_desc"] = "Signals stop / drive",
+        ["prSig3_name"] = "Prussian signal - 3 Flügel",
+		["prSig3_desc"] = "Signals stop / drive",
+        ["prSig31_name"] = "Prussian signal - 3 Flügel",
+		["prSig31_desc"] = "Signals stop - drive/junction",
+        ["prSig33_name"] = "Prussian signal - 3 Flügel",
+		["prSig33_desc"] = "Signals stop - drive/different junction",
+        ["prVSig0_name"] = "Prussian pre-signal",
+        ["prVSig0_desc"] = "Signals expect stop",
+        ["prVSig1_name"] = "Prussian pre-signal",
+        ["prVSig1_desc"] = "Signals expect driving",
+	},
+	de = {
+		["mod_name"] = "Preussische Formsignale",
+		["mod_desc"] = "Frühe Signale der KPEV. Früher dienten die Signale nicht der Geschwindigkeitsangabe sondern der Sicherung der Fahrwege. Daher gab es bis zu 3-flügelige Einfahrsignale, die die Fahrten in verschiedene Gleise signalisierten. Wichtig war vor allem der oberste Flügel, der die Fahrt frei gab oder Halt anzeigte. Daher hatte das Vorsignal auch nur die Funktion diesen Begriff anzukündigen. Ab 1910 änderte sich die Signalordnung und es wurden neue Farbanstriche eingeführt.\nEs gab noch keine Gasflaschen zur Signalbeleuchtung. Tagsüber wurden die Petroleumlampen heruntergekurbelt und gelöscht. Nur bei Dunkelheit oder tagsüber bei unsichtigem Wetter brannten sie am Mast.",
+
+		["prSig1_name"] = "Formsig Preu Hp0/Hp1 Flügel: 1",
+		["prSig1_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/Fahrt",
+        ["prSig2_name"] = "Formsig Preu Hp0/Hp2 Flügel: 2",
+		["prSig2_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/Abzweigung",
+        ["prSig21_name"] = "Formsig Preu Hp0/Hp1 Flügel: 2",
+		["prSig21_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/Fahrt",
+        ["prSig3_name"] = "Formsig Preu Hp0/Hp1 Flügel: 3",
+		["prSig3_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/Fahrt",
+        ["prSig31_name"] = "Formsig Preu Hp0/Hp2 Flügel: 3",
+		["prSig31_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/Abzweigung",
+        ["prSig33_name"] = "Formsig Preu Hp0/Hp3 Flügel: 3",
+		["prSig33_desc"] = "Preußisches Hauptsignal. Wegsignalisierung Halt/andere Abzweigung",
+        ["prVSig0_name"] = "Form Vsig Preu Vr0/Vr0",
+        ["prVSig0_desc"] = "Preußisches Vorsignal. Wegsignalisierung Halt erwarten/Halt erwarten",
+        ["prVSig1_name"] = "Form Vsig Preu Vr0/Vr1",
+        ["prVSig1_desc"] = "Preußisches Vorsignal. Wegsignalisierung Halt erwarten/Fahrt erwarten",
+	},
+}
+end
